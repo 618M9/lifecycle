bb591621f3fa64a444ceab76fc56e18b0e3cf2c8
Shafiq <ishafiqul801@gmail.com>
2018-05-28 02:06:44 -0700

    Activitylifecycle

---
+++ app/src/main/java/com/aaa/MainActivity.java
@@ -0,0 +1,46 @@
+package com.aaa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.i("I am in :","onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("I am in :","onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("I am in :","onStop");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("I am in :","onpause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("I am in :","onResume");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("I am in :","onCreate");
+    }
+}
